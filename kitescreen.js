8e1d3ed94e6ae41e3cfe130499b4ddb6c63981f8
Joel stransky <stranskydesign@gmail.com>
2019-11-28 15:17:14 -0800

    discover all video and assign events

--- kitescreen.js
+++ kitescreen.js
@@ -1,18 +1,23 @@
+let state = false;
+
 chrome.browserAction.onClicked.addListener(function(tab) {
+  if (!state) {
+    chrome.tabs.insertCSS(null, { file: "kitescreen.css" });
     // No tabs or host permissions needed!
-    console.log('Turning ' + tab.url + ' red!');
+    console.log("Turning " + tab.url + " red!");
     chrome.tabs.executeScript({
-      code: `(async () => {
-      var videoElement = document.getElementsByTagName("video")[0];
-      try {
-        if (videoElement !== document.pictureInPictureElement) {
-          await videoElement.requestPictureInPicture();
-        } else {
-          await document.exitPictureInPicture();
-        }
-      }
-      catch(error) {}
-    })()`
+      file: "extension.js",
+      allFrames: true
     });
-  });
-  
+  } else {
+    var videoElement = document.getElementsByTagName("video")[0];
+    // try {
+    //   if (videoElement !== document.pictureInPictureElement) {
+    //     await videoElement.requestPictureInPicture();
+    //   } else {
+    //     await document.exitPictureInPicture();
+    //   }
+    // } catch (error) {}
+  }
+  state = !state;
+});
